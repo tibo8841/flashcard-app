c2e6c8823ddb4c8b65fab499b437f11c496bfa9f
tibo8841 <thibaut.hucker@gmail.com>
2023-01-24 02:18:42 +0000

    started implementing how notes will work

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,12 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="note">
+        <div className="note-title"></div>
+        <div className="note-content"></div>
       </div>
     </div>
   );
