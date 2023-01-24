1b5858838924af43fb9b8c658bcf5e681c9e7d1c
tibo8841 <thibaut.hucker@gmail.com>
2023-01-24 02:32:20 +0000

    implemented some basic styling of notes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,8 +5,12 @@ function App() {
   return (
     <div className="App">
       <div className="note">
-        <div className="note-title"></div>
-        <div className="note-content"></div>
+        <div className="note-title">
+          <p>this is title</p>
+        </div>
+        <div className="note-content">
+          <p>this is content of the note</p>
+        </div>
       </div>
     </div>
   );
